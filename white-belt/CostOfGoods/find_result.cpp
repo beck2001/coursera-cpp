b4898ea4dab49de3f86f174229990fa2f3e5fe19
Muratbek Sharibekov <84804906+beck2001@users.noreply.github.com>
2022-03-08 00:24:38 +0600

    Solved task for finding resulting cost of goods

---
+++ white-belt/CostOfGoods/find_result.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+double find_percentage(double value, double percentage);
+
+int main() {
+    double n, a, b, x, y, result;
+    std::cin >> n >> a >> b >> x >> y;
+    double first_discount = n, second_discount = n;
+
+    if (n > a) {
+        first_discount = n - find_percentage(n, x);
+    }
+
+    if (n > b) {
+        second_discount = n - find_percentage(n, y);
+    }
+
+    result = first_discount < second_discount ? first_discount : second_discount;
+    std::cout << result;
+}
+
+double find_percentage(double value, double percentage) {
+    return value * percentage / 100;
+}
