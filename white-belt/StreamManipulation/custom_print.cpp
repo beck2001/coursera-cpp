36922fed8f6be70ed12d9976f0447fe0951fda86
Muratbek Sharibekov <84804906+beck2001@users.noreply.github.com>
2022-04-13 18:12:17 +0600

    Optimized solution without usage of vectors

--- white-belt/StreamManipulation/custom_print.cpp
+++ white-belt/StreamManipulation/custom_print.cpp
@@ -2,8 +2,6 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
-#include <vector>
-#include <sstream>
 
 void custom_print(const std::string &file_path);
 
@@ -15,26 +13,14 @@ int main() {
 void custom_print(const std::string &file_path) {
     std::ifstream input(file_path);
     if (input) {
-        int n, m, k = 0;
-        std::string number;
-        std::vector<std::string> numbers;
-        input >> n;
-        input.ignore(1);
-        input >> m;
-        input.ignore(1);
-
-        while (std::getline(input, number)) {
-            std::stringstream ss(number);
-            while (std::getline(ss, number, ',')) {
-                numbers.push_back(number);
-            }
-        }
-
+        int n, m;
+        input >> n >> m;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                if (k != n * m) {
-                    std::cout << std::setw(10) << numbers[k++];
-                }
+                int value;
+                input >> value;
+                input.ignore(1);
+                std::cout << std::setw(10) << value;
                 if (j != m - 1) {
                     std::cout << " ";
                 }
@@ -43,7 +29,7 @@ void custom_print(const std::string &file_path) {
                 std::cout << std::endl;
             }
         }
-    }
+    }   
     else {
         std::cout << "error while reading file" << std::endl;
     }
